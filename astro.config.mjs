c48bdd5a8709935b47147a66243a8170c105fe28
GnoxNahte <gnoxnahte.dev@gmail.com>
2023-03-26 18:54:13 +0800

    Add MDX support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://gnoxnahte.com/',
+  site: 'https://gnoxnahte.com/',
+  integrations: [mdx()]
 });
