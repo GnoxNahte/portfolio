abd7a1a8226f2788fb7990aaeed5927621c23465
GnoxNahte <gnoxnahte.dev@gmail.com>
2023-08-29 22:48:46 +0800

    Revert "Try deploying to Vercel"
    This reverts commit 1032e3b83341894282ccef45700f83e244e7b411.
    Having some problems when running locally

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 import { defineConfig } from 'astro/config';
-import mdx from "@astrojs/mdx";
 
-import vercel from "@astrojs/vercel/serverless";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://gnoxnahte.com/',
-  integrations: [mdx()],
-  output: "server",
-  adapter: vercel()
+  integrations: [mdx()]
 });
