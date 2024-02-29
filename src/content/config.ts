dc7d9635e375e27e6de3b96f8c29206f5f8b91d2
GnoxNahte <gnoxnahte.dev@gmail.com>
2024-02-29 17:14:14 +0800

    Add Date to project cards and modify date on project pages.
    Now shows something like:
    21 August 2022 - 28 August 2022
    or
    30 January 2023 - Ongoing
    
    Adds the duration for project pages and if isn't Ongoing

--- src/content/config.ts
+++ src/content/config.ts
@@ -7,7 +7,7 @@ const projects = defineCollection({
   schema: ({ image }) => z.object({
     title: z.string(),
     shortDescription: z.string(), 
-    startDate: z.date().optional(),
+    startDate: z.date(),
     releaseDate: z.date().optional(),
     projectLink: z.string().url().optional(),
     githubLink: z.string().url().optional(),
